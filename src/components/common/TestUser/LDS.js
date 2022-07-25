3b8e3519aec3d2fe2a90975a661bbf12cb7147ce
Leedun <313lds@naver.com>
2022-07-25 22:42:49 +0900

    test

--- src/components/common/TestUser/LDS.js
+++ src/components/common/TestUser/LDS.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import HalfRating from "components/review/HalfRating";
 const LDS = ()=> {
     return (
       <div>
           <h1>이덕수 테스트 페이지</h1>
+          <HalfRating/>
+
       </div>
     );
   }
