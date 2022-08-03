e47c7ff59a548355d6be0aa8a2d13f6f24cbeb89
JunHee Myung <platinyan@naver.com>
2022-08-03 14:23:56 +0900

    로딩스피너 common으로 경로변경

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import reportWebVitals from "reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import {GlobalProvider} from 'components/common/GlobalProvider';
 import TestPage from 'components/common/TestPage';
-import LoadingSpinner from 'components/member/LoadingSpinner';
+import LoadingSpinner from 'components/common/LoadingSpinner';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
