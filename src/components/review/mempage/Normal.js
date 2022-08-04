b022a18a16d795b8c289eb1f2a87d124f83a27f1
Leedun <313lds@naver.com>
2022-08-04 13:05:22 +0900

    11

--- src/components/review/mempage/Normal.js
+++ src/components/review/mempage/Normal.js
@@ -1,8 +1,4 @@
 import React from 'react'
-import { useContext } from "react";
-import { GlobalContext } from "components/common/GlobalProvider";
-import { Route, Router, Routes } from 'react-router-dom';
-
 
 
 export default function Normal() { 
