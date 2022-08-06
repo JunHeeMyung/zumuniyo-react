7ec7be542bd636fe933e9dc12d06b49e0c49f62c
Leedun <313lds@naver.com>
2022-08-06 15:22:55 +0900

    11

--- src/components/review/mempage/normal/OrderList.js
+++ src/components/review/mempage/normal/OrderList.js
@@ -1,9 +1,54 @@
-import React from 'react'
+// import React from 'react'
+import React, { useState, useEffect, useContext } from "react";
+import { GlobalContext } from "components/common/GlobalProvider";
+import ReviewInsert from "components/review/components/ReviewInsert";
 
 export default function OrderList() {
+  const { globalAxios } = useContext(GlobalContext);
+
+  
+  const [orders, setOrders] = useState([]);
+
+const ordersList = () => {
+    globalAxios('/review/orderList', 'post', {}, res => {      
+      if (res) {
+        console.log(res);        
+        setOrders(res);                   
+        
+      } else {
+        alert("failed to ");
+      }
+    });
+  }
+
+  // useEffect(ordersList, []);
+
+  
+
+
+  function convertDate(longValue) {
+    return new Date(longValue).toLocaleString();
+  }
+  useEffect(ordersList, []);
+
   return (
-    <div>
+    <>
+      <h1>주문목록</h1>      
+      {/* {f()} */}
       
-    </div>
+      <ReviewInsert/>
+      
+      {orders.map((order, index) =>{ 
+          return  <div key={index}>            
+              <p>{order.orderGroupSeq}</p>
+              <p>{order.tableNum}</p>
+              <p>{order.orderStatus}</p>
+              <p>{convertDate(order.orderGroupRegdate)}</p>
+            </div> 
+          })}
+
+ 
+      
+    </>
   )
 }
