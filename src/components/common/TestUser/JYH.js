fb60b31a521e29686e4a173621668ef5ebf38611
DESKTOP-LE375VU\yhj <yhju765@gmail.com>
2022-08-11 04:34:55 +0900

    쿠폰기능 받기위해 우선 커밋... 220811

--- src/components/common/TestUser/JYH.js
+++ src/components/common/TestUser/JYH.js
@@ -3,9 +3,13 @@ import React from "react";
 
 import Menu from "components/menu/Menu";
 import MenuQR from "components/menu/MenuQR";
+import MenuListQR from "components/menu/MenuListQR";
+
 import { Route, Routes,Link } from "react-router-dom";
 
 import AuthChecker from "components/common/auth/AuthChecker";
+
+
 
 
 const JYH = ()=> {
@@ -16,6 +20,7 @@ const JYH = ()=> {
           <Link to="/"><button>홈으로</button></Link>
           <Link to="menu"><button>메뉴</button></Link>
           <Link to="menuQR/4883/1"><button>QR메뉴 - 4883</button></Link>
+          <Link to="menuQRnew/4882/1"><button>QR메뉴collaps형태 - 4882</button></Link>
           <Link to="menu/4882"><button>메뉴4882</button></Link>
           <Link to="menu/4883"><button>메뉴4883</button></Link>
             <hr/>
@@ -24,6 +29,7 @@ const JYH = ()=> {
               <Route path="/" element={<><h1>정영훈 테스트 페이지</h1></>} />
               <Route path="/menu/*" element={<Menu/>} />
               <Route path="/menuQR/:shopSeq/:tableNum" element={<AuthChecker><MenuQR /></AuthChecker>} />
+              <Route path="/menuQRnew/:shopSeq/:tableNum" element={<AuthChecker><MenuListQR /></AuthChecker>} />
               <Route path="*" element={<><h1>주소값 이상</h1></>} />
             </Routes>
           </div>
