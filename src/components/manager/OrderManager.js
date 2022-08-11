4cfded1a8d639e703980e9d1fcc6525bced70499
JunHee Myung <platinyan@naver.com>
2022-08-11 14:14:42 +0900

    주소병합

---
+++ src/components/manager/OrderManager.js
@@ -0,0 +1,30 @@
+import React,{ useEffect} from "react";
+import moment from "moment-timezone"
+
+
+const OrderManager = (props)=> {
+    
+    moment.DATETIME_LOCAL = "YYYY-MM-DDTHH:mm:ss";
+
+
+
+
+
+   
+    
+
+
+    useEffect(
+        () => { 
+           
+        },[]
+      );
+
+    return (
+      <>
+            주문관리자
+
+      </>
+    );
+  }
+  export default OrderManager;
